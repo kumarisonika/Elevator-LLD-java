899ae90ba8e5b1c919128024b2e0af22fdf1e879
Kumari Sonika <kumarisonika1929@gmail.com>
2025-12-24 03:00:53 +0530

    request class

---
+++ src/main/java/org/example/ElevatorState.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public enum ElevatorState {
+    IDLE,
+    MOVING_UP,
+    MOVING_DOWN
+}
---
+++ src/main/java/org/example/Request.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Request {
+    int sourceFloor;
+    Integer destinationFloor;
+    private Direction direction;
+
+    public  Request(int sourceFloor, Direction direction){
+        this.sourceFloor= sourceFloor;
+        this.direction= direction;
+        destinationFloor=null;
+    }
+
+    public Request(int sourceFloor, int destinationFloor){
+        this.sourceFloor= sourceFloor;
+        this.destinationFloor= destinationFloor;
+        this.direction=null;
+    }
+
+    public int getSourceFloor(){ return sourceFloor;}
+
+    public Direction getDirection(){ return direction;}
+
+    public int getDestinationFloor(){ return destinationFloor;}
+}
